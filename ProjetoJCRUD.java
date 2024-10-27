eb1dfe53f9495b190e419395cfcbd31cb4f4a447
LUCAS MATHEUS <138603411+lucasm9140@users.noreply.github.com>
2024-10-27 00:01:51 -0300

    Projeto crud, tela + modulo de conxexao

---
+++ ProjetoJCRUD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetojcrud;
+
+/**
+ *
+ * @author lucas
+ */
+public class ProjetoJCRUD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
